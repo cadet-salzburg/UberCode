f77dceb5dbbe9780189a3939ac1c3d39d27518de
Nikolas Psaroudakis <psaroudakis@gmail.com>
2012-11-07 10:27:28 +0100

    adding ui elements

--- build/vc10/UbImageBlock.cpp
+++ build/vc10/UbImageBlock.cpp
@@ -2,13 +2,44 @@
 
 namespace Uber {
 	UbImageBlock::UbImageBlock( QGraphicsItem *parent )
+		:m_Node(0),
+		m_Image( new UbImage() )
 	{
 
 	}
-
-
 	UbImageBlock::~UbImageBlock(void)
 	{
 
+	}
+	void UbImageBlock::setNode( UbOutletNode *node )
+	{
+		m_Node = node;
+	}
+	void UbIOBlock::updateData(_2Real::app::AppData data) 
+	{
+		m_Data = data;
+		if ( m_Node && m_Node->type() == Uber::OutputNodeType )
+		{
+			UbOutletNode *m_OutletNode = 0;
+			m_OutletNode =  dynamic_cast<UbOutletNode*>(m_Node);
+
+
+			try
+			{
+				if( m_OutletNode->getHandle().getTypename() == "number image" )
+				{
+					Image const& img = m_Data.getData< Image >();
+					QImage q( img.getData(), img.getWidth(), img.getHeight(), 3*img.getWidth(), QImage::Format_RGB888 );
+					//QImage q2 = q.copy();
+					dynamic_cast<UbImage*>(m_ValueWidget)->setImage(q);
+					m_ProxyWidget->update();
+				}
+				update();
+			}
+			catch(Exception& e)
+			{
+				cout << e.message() << e.what() << std::endl;
+			}
+		}
 	}
 }
--- build/vc10/UbImageBlock.h
+++ build/vc10/UbImageBlock.h
@@ -1,12 +1,25 @@
 #pragma once
 #include <QGraphicsObject>
 #include "UbOutletNode.h"
+#include "UbImage.h"
+#include "app/_2RealAppData.h"
+
 namespace UberCode {
 	class UbImageBlock :public QGraphicsObject
 	{
 	public:
 		UbImageBlock( QGraphicsItem *parent );
 		~UbImageBlock(void);
+		void			setNode(UbOutletNode *node );
+
+	private:
+		UbOutletNode			*m_Node;
+		UbImage					*m_Image;
+		_2Real::app::AppData	m_Data;
+	public slots:
+			void					updateData(_2Real::app::AppData data);
+	signals:
+			void					sendData(_2Real::app::AppData data);
 	};
 }
 
